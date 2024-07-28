506545a9daedcddf655828503743368668f75ac8
baotq22 <87086420+baotq22@users.noreply.github.com>
2024-07-28 23:49:28 +0700

    remove json to avoid error image when deploy (social)

--- src/components/pages/social-links.jsx
+++ src/components/pages/social-links.jsx
@@ -1,35 +1,43 @@
+import fb from "../../assets/social-icon/fb.png"
+import ig from "../../assets/social-icon/ig.png"
+import li from "../../assets/social-icon/li.png"
+import rd from "../../assets/social-icon/rd.png"
+import sc from "../../assets/social-icon/sc.png"
+import tt from "../../assets/social-icon/tt.svg"
+import yb from "../../assets/social-icon/yb.png"
+
 export const SocialLinks = () => {
     const socialLinksJSON = [
         {
-            "src": "src/assets/social-icon/fb.png",
+            "src": fb,
             "link": "https://www.facebook.com/tuong.quan.22/"
         },
         {
-            "src": "src/assets/social-icon/ig.png",
+            "src": ig,
             "link": "https://www.instagram.com/dorino__/"
         },
         {
-            "src": "src/assets/social-icon/li.png",
+            "src": li,
             "link": "https://www.linkedin.com/in/baoqtran22/"
         },
         {
-            "src": "src/assets/social-icon/rd.png",
+            "src": rd,
             "link": "https://www.reddit.com/user/Live-Month-2949/"
         },
         {
-            "src": "src/assets/social-icon/sc.png",
+            "src": sc,
             "link": "https://soundcloud.com/ang-c-p-nh-t-6/"
         },
         {
-            "src": "src/assets/social-icon/tt.svg",
+            "src": tt,
             "link": "https://www.tiktok.com/@kenneth.chan.22/"
         },
         {
-            "src": "src/assets/social-icon/yb.png",
+            "src": yb,
             "link": "https://www.youtube.com/channel/UCSxQcqfblnoGVgt83DpFH0Q/"
         }
     ]
-    
+
     return (
         <div className="social">
             <div className="title">SOCIAL LINKS</div>
